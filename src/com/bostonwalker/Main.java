e8265b3befb06eaa171842a3c2ce2218066fe18a
raja <raja.amirapu@gmail.com>
2019-12-22 11:40:43 +0530

    'changes'

--- src/com/bostonwalker/Main.java
+++ src/com/bostonwalker/Main.java
@@ -11,7 +11,7 @@ import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.features2d.FeatureDetector;
 import org.opencv.features2d.KeyPoint;
-import org.opencv.highgui.Highgui;
+import org.opencv.highgui.Highgui;  
 
 public class Main {
 
@@ -25,7 +25,7 @@ public class Main {
 
         Mat srcImgMat = Highgui.imread(sourcePath);
         Mat srcImgBlank = Highgui.imread(blankPath);
-        
+
 
         if (srcImgMat == null)
         {
@@ -36,7 +36,7 @@ public class Main {
         System.out.println("Loaded image at " + sourcePath);
 
         MatOfKeyPoint matOfKeyPoints = new MatOfKeyPoint();
-       
+
         FeatureDetector blobDetector = FeatureDetector.create(FeatureDetector.ORB);
         blobDetector.detect(srcImgMat, matOfKeyPoints);
 
@@ -47,14 +47,14 @@ public class Main {
         for(KeyPoint kp:lstKp)
         {
         	//MatOfKeyPoint mkp=new MatOfKeyPoint();
-        	
+
         	//System.out.println(kp.pt.x);
         	//System.out.println(kp.pt.y);
         	Core.circle(srcImgMat, kp.pt, 2, new Scalar(255.0,0,0));
-        
+
         }
-        
-        
+
+
         Highgui.imwrite("E:\\RAJA\\opencv-2.4.9\\picturestobeanalyzed\\modified" + System.currentTimeMillis() +  ".jpg",srcImgMat);
        //Core.
         //System.out.println(matOfKeyPoints.toList());
@@ -66,48 +66,48 @@ public class Main {
         //Core.line(srcImgBlank, matOfKeyPoints., pt2, color)
      //MinMaxLocResult minMax=   Core.minMaxLoc(srcImgMat);
      Core.line(srcImgBlank, new Point(0,20), new Point(1200,20), new Scalar(70,0));
-     
+
      for(KeyPoint kp:lstKp)
      {
      	//System.out.println(kp.pt.x);
      	//System.out.println(kp.pt.y);
     	 //MatOfPoint mp=new MatOfPoint(kp.pt.x,kp.pt.y);
-    	 
-    			 
+
+
      	Core.circle(srcImgBlank, kp.pt, 2, new Scalar(255.0));
-    	 
-  
+
+
      }
      Highgui.imwrite("E:\\RAJA\\opencv-2.4.9\\picturestobeanalyzed\\line.jpg",srcImgBlank);
-     
+
         /*for(int row=0;row<=rows; row++)
         {
         	for(int col=0;col<=cols; col++)
         	{
-        	
+
         		double[] width=matOfKeyPoints.get(row, col);
         		if(width!=null)
         		{
 	        		for(int i=0;i<width.length;i++)
 	        		{
 	        			lst.add(width[i]);
-	        			
+
 	        		}
         		}
         		dblList.add(lst);
         	//	System.out.println(width);
         	//	dblList=Arrays.asList(width);
-        		
+
         		//System.out.println(dblList);
         }
         }*/
         System.out.println(dblList);
         //int height=matOfKeyPoints.height();
-        
+
    //    System.out.println(width);
      //   System.out.println(height);
-        
-        		
+
+
         //List<KeyPoint> keyPoints = matOfKeyPoints.toList();
 
     }
